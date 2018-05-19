cebef320e8863b613096db1d9d0502a659d53a2f
Eros Gonzalez Lopez <egonzalez-lopez@csumb.edu>
2018-05-19 05:08:11 +0000

    Heroku Link

--- database.php
+++ database.php
@@ -10,13 +10,13 @@ function getDBConnection() {
     $password = "";
     
     //when connecting from Heroku
-    // if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-    //     $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    //     $host = $url["host"];
-    //     $dbName = substr($url["path"], 1);
-    //     $username = $url["user"];
-    //     $password = $url["pass"];
-    // } 
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbName = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    } 
     
     try {
         //Creates a database connection
